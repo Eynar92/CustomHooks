b85936d60149078ea53c1ce95bb3ba8117e64cda
Joel Alvarez <eynar.j.alvarez.m@gmail.com>
2024-02-12 18:22:59 -0400

    First commit

---
+++ useTodos.js
@@ -0,0 +1,52 @@
+import { useEffect, useReducer } from "react";
+import { todoReducer } from "../08-useReducer/todoReducer";
+
+export const useTodos = () => {
+    const initialState = [];
+
+    const init = () => {
+        return JSON.parse(localStorage.getItem('todos')) || [];
+    }
+
+    const [todos, dispatchTodo] = useReducer(todoReducer, initialState, init);
+
+    useEffect(() => {
+        localStorage.setItem('todos', JSON.stringify(todos));
+    }, [todos]);
+
+    const totalTodo = todos.length;
+
+    const pendingTodosCount = todos.filter(todo => !todo.done).length;
+
+    const handleNewTodo = (todo) => {
+        const action = {
+            type: '[TODO] Add Todo',
+            payload: todo,
+        }
+
+        dispatchTodo(action);
+    }
+
+    const handleToggleTodo = (id) => {
+        dispatchTodo({
+            type: '[TODO] Toggle Todo',
+            payload: id,
+        })
+    }
+
+    const handleDeleteTodo = (id) => {
+        dispatchTodo({
+            type: '[TODO] Remove Todo',
+            payload: id,
+        });
+    }
+
+    return {
+        todos,
+        totalTodo,
+        pendingTodosCount,
+        handleNewTodo,
+        handleToggleTodo,
+        handleDeleteTodo,
+    }
+}
